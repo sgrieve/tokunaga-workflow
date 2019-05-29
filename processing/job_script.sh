cfd35dc7f41497577b1bacfe8e41a6a9cfb7f475
Stuart Grieve <s.grieve@qmul.ac.uk>
2019-05-29 11:04:14 +0100

    need to load proj separately to gdal

--- processing/job_script.sh
+++ processing/job_script.sh
@@ -15,8 +15,9 @@
 module load gdal/2.3.1
 module load gcc/6.3.0
 module load python/3.6.3
+module load proj/5.2.0
 
-Parse parameter file to get variables.
+# Parse parameter file to get variables.
 number=$SGE_TASK_ID
 paramfile=/data/home/faw513/tokunaga-workflow/processing/array_params_0_1.txt
 
