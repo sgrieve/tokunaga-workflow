8aa3d876c5d93a6fc4057b77d8865bb070f5ed0e
Stuart Grieve <s.grieve@qmul.ac.uk>
2019-06-14 12:47:42 +0100

    adding a helper script to combine json files

---
+++ analysis/joiner.py
@@ -0,0 +1,15 @@
+import json
+from glob import glob
+
+files = glob('jsons/*.json')
+
+joined_data = {}
+
+for f in files:
+    with open(f) as js:
+        data = json.load(js)
+
+    joined_data.update(data)
+
+with open('merged_precip.json', 'w') as outfile:
+    json.dump(joined_data, outfile)
