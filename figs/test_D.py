48b28ffea757d5acada5ca1779b6aa6284939018
Stuart Grieve <s.grieve@qmul.ac.uk>
2019-07-02 12:32:20 +0100

    getting the D code to check koppen zones too

--- figs/test_D.py
+++ figs/test_D.py
@@ -2,20 +2,33 @@ import tokunaga_fns as toku
 from glob import glob
 import numpy as np
 
-file_list = glob('../data/TokunagaData_*_*.csv')
+arid = glob('../data/TokunagaData_[4-7]_*.csv')
+tropical = glob('../data/TokunagaData_[1-3]_*.csv')
+temperate = glob('../data/TokunagaData_8_*.csv')
+temperate += glob('../data/TokunagaData_11_*.csv')
+temperate += glob('../data/TokunagaData_14_*.csv')
+cold = glob('../data/TokunagaData_17_*.csv')
+cold += glob('../data/TokunagaData_21_*.csv')
+cold += glob('../data/TokunagaData_25_*.csv')
 
-Ds = []
-for filename in file_list:
+labels = ['Arid', 'Tropical', 'Temperate', 'Cold']
+All_Data = []
 
-    _, strahler_data, lengths = toku.read_toku_data(filename)
+for i, data in enumerate([arid, tropical, temperate, cold]):
+    Ds = []
+    for filename in data:
 
-    Rr_mean, Rr_std = toku.calc_Rr(strahler_data, lengths)
-    Rb_mean, Rb_std = toku.calc_Rb(strahler_data)
+        _, strahler_data, lengths = toku.read_toku_data(filename)
 
-    D = toku.D(Rb_mean, Rr_mean)
-    if Rr_mean < 1.05:
-        continue
+        Rr_mean, Rr_std = toku.calc_Rr(strahler_data, lengths)
+        Rb_mean, Rb_std = toku.calc_Rb(strahler_data)
 
-    Ds.append(D)
+        if Rr_mean < 1.05:
+            continue
 
-print('Mean: {} StdDev: {}'.format(np.mean(Ds), np.std(Ds)))
+        D = toku.D(Rb_mean, Rr_mean)
+        Ds.append(D)
+        All_Data.append(D)
+
+    print('{} Mean: {} StdDev: {}'.format(labels[i], np.mean(Ds), np.std(Ds)))
+print('All Data Mean: {} StdDev: {}'.format(np.mean(All_Data), np.std(All_Data)))
