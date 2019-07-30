44f2bed23a7ffa73c55bf2e8d964741a6bc62720
Stuart Grieve <s.grieve@qmul.ac.uk>
2019-07-30 10:18:02 +0100

    adding job scripts for a bunch of plots

---
+++ figs/job-10.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -cwd
+#$ -j y
+#$ -N toku-figs10
+#$ -o /data/Geog-c2s2/toku/
+#$ -pe smp 1
+#$ -l node_type=dn
+#$ -l h_vmem=10G
+#$ -l h_rt=20:00:0
+
+module load python/3.6.3
+module load gdal/2.3.1
+module load proj/5.2.0
+
+source /data/home/faw513/toku-fig-env/bin/activate
+
+python /data/home/faw513/tokunaga-workflow/figs/precip_a_c.py 0.9
