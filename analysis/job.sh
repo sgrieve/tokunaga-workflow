58f94a231dc81cdea6a1a88a65449fdb94107c2c
Stuart Grieve <s.grieve@qmul.ac.uk>
2019-08-14 16:37:10 +0100

    addomg a job script for the ac code

---
+++ analysis/job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -cwd
+#$ -j y
+#$ -N a_c_dict
+#$ -o /data/Geog-c2s2/toku/
+#$ -pe smp 1
+#$ -l node_type=dn
+#$ -l h_vmem=2G
+#$ -l h_rt=4:00:0
+
+module load python/3.6.3
+module load gdal/2.3.1
+module load proj/5.2.0
+
+source /data/home/faw513/toku-env/bin/activate
+
+python /data/home/faw513/tokunaga-workflow/analysis/a_c_dict.py
